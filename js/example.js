4ad6a9ea2a3b7efec7851bf289421652d3a040eb
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 21:04:40 -0400

    Added all files; First Commit

---
+++ js/example.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+
+        var table = 3;
+        var operator = 'addition';
+        var i = 1;
+        var msg = '';
+
+        if (operator === 'addition') {
+            // Do addition
+        while (i < 11) {
+            msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+            }
+        }
+        else {
+            // Do multiplication
+            while (i < 11) {
+                msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+                i++;
+            }
+        }
+
+        // Write the message into the page
+        var el = document.getElementById('blackboard');
+        el.innerHTML = msg;
+
+}) ();
